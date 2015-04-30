4b823b41f5d9ccfd7a7df168ae64c133bf6918a3
Scott Stewart <wsstewart@gmail.com>
2015-04-30 12:59:24 -0400

    adding code!

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # nepal_quake_youtube
-A simple geocoding exercise for youtube videos geotagged in nepal, since the quake
+A simple geocoding exercise for youtube videos geotagged in nepal, since the quake. Since the youtube api does not report locations as lats and lons, I developed a heuristic approach, which uses search radius and 'quad trees' to refine video location.
+
+
+-- youtube_geocoder.js: a node based app to retrieve the urls
+
+USAGE: 
+
+node youtube_geocode.js <<text_to_find>> <<hours_to_sleep>> <<bbox_lonmin>> <<bbox_latmin>> <<bbox_lonmax>> <<bbox_latmax>>  <<minradius>> <<time_to_start>> <<apikey>>
+
+
+text_to_find: text string to search for
+hours_to_sleep: hours to sleep between trying to find new videos
+bbox_lonmin: longitude min of bbox to search
+bbox_latmin: latitude min of bbox to search
+bbox_lonmax: longitude max of bbox to search
+bbox_latmax: latitude max of bbox to search
+minradius: the min  (in km) from a point radius at which we will attribute a video to a point
+time_to_start: only find videos published after this time
+apikey: the youtube apikey to use
+
+
+example:
+
+Find all the earthquake videos within the bounding box for nepal, after the event time. Attribute when our search radius < 0.5 km. Sleep for 4 hours between calls to find new videos
+
+node youtube_geocoder.js earthquake 4 80.00 25.767 88.183 30.45 0.5 2015-04-25T06:45:00Z  AIzaSyDS4heuflAGO1bE67aZioeT3OE
+
+
+OUTPUT: a tab separated file (written to stdout via console.log) with the videos and locations.
+
+DEPENDENCIES: turf-distance, sync-request, array_extended node modules
+
+KNOWN BUGS: does no error checking on command line args and assumes they will all be present.
---
+++ youtube_geocoder.js
@@ -0,0 +1,201 @@
+var request = require("sync-request");
+var turf = require("turf-distance");
+var array = require("array-extended");
+
+
+/***************************
+GET THE COMMAND LINE VARS
+****************************/
+// get text (tag) to search for 
+var text = process.argv[2];
+
+//get the number of hours to sleep
+var gHours = process.argv[3];
+
+//get the longitude min of BBOX
+var lonmin = parseFloat(process.argv[4]);
+
+//get the latitude min of BBOX
+var latmin = parseFloat(process.argv[5]);
+
+//get the longitude max of BBOX
+var lonmax = parseFloat(process.argv[6]);
+
+//get the latitude max of BBOX
+var latmax = parseFloat(process.argv[7]);
+
+//get max distance from the radius, in km, to report results
+var gMaxDistance = process.argv[8];
+
+//get the start time (publishedAfter) to begin at
+var startTime = process.argv[9];
+
+//get the start time (publishedAfter) to begin at
+var key = process.argv[10];
+
+//units are always km
+var gUnits = 'kilometers';
+var resultsArray = new Array();
+
+/*
+outputElement - output an element (video) if we havent seen it before (remove dups)
+*/
+function outputElement(item, longitude, latitude)
+{
+	id = item.id.videoId;
+	
+	urlTemp = 'https://www.googleapis.com/youtube/v3/videos?id='+id+'&part=snippet&type=video&key='+key;
+	var response = request('GET',urlTemp);
+	var body = response.body.toString('utf-8');
+	var info = JSON.parse(body);
+	item = info.items[0];
+
+	if (resultsArray.indexOf(id) < 0)
+	{
+		resultsArray.push(id);
+		videoURI = "https://www.youtube.com/watch?v="+id;
+		dateTime = item.snippet.publishedAt;
+		title = item.snippet.title;
+		ft = new Date();
+		console.log(id+'\t'+longitude+'\t'+latitude+'\t'+title+"\t"+videoURI+"\t"+dateTime+"\t"+ft);
+	}
+}
+
+/*
+findCircumcircle - find the min circumcircle for a bounding box
+*/
+function findCircumcircle(lonmin,latmin,lonmax,latmax)
+{
+	var circle = {loncenter:0, latcenter:0, radius: 0};
+	loncenter = (lonmax+lonmin)/2;
+	latcenter = (latmax+latmin)/2;
+	
+	a = lonmax - loncenter;
+	b = latmax - latcenter;
+	
+	c = Math.sqrt((a*a)+(b*b));
+	
+	circle.loncenter = loncenter;
+	circle.latcenter = latcenter;
+	circle.radius = c;
+	
+	return circle;
+}
+
+/*
+distanceOfRadius - using turf, find the distance between two points (the center of a circle, and a point along the circumference)
+*/
+function distanceOfRadius(lon1,lat1,lon2,lat2,units)
+{
+	var point1 = {
+	  "type": "Feature",
+	  "properties": {},
+	  "geometry": {
+		"type": "Point",
+		"coordinates": [lon1, lat1]
+	  }
+	};
+	var point2 = {
+	  "type": "Feature",
+	  "properties": {},
+	  "geometry": {
+		"type": "Point",
+		"coordinates": [lon2, lat2]
+	  }
+	};
+
+	return turf(point1, point2, units);
+}
+
+/*
+findAllVideosBBox - find all the youtube videos within a bounding box, recursively using a quadtree
+*/
+function findAllVideosBBox(lonmin,latmin,lonmax,latmax,pubDate)
+{
+ 	var circle = findCircumcircle(lonmin,latmin,lonmax,latmax);
+ 	var distance = distanceOfRadius(circle.loncenter,circle.latcenter,circle.loncenter,(circle.latcenter+circle.radius),gUnits);
+ 
+	urlTemp = 'https://www.googleapis.com/youtube/v3/search?locationRadius='+distance+'km&q='+text+'&part=id&maxResults=1&location='+circle.latcenter+','+circle.loncenter+'&type=video&key='+key+'&publishedAfter='+pubDate;
+
+	var response = request('GET',urlTemp);
+	var body = response.body.toString('utf-8');
+	var info = JSON.parse(body);
+	var items;
+	if (info.items)
+	{
+		if (distance <= gMaxDistance)
+		{
+			for (var t = 0; t < info.items.length; t++)
+			{
+				outputElement(info.items[t],circle.loncenter,circle.latcenter);
+			}
+			nextPageToken = info.nextPageToken;
+			while(nextPageToken)
+			{
+				urlTemp = 'https://www.googleapis.com/youtube/v3/search?locationRadius='+distance+'km&q='+text+'&part=id&maxResults=50&location='+circle.latcenter+','+circle.loncenter+'&type=video&key='+key+'&publishedAfter='+pubDate+'&pageToken='+nextPageToken;
+				
+				var response = request('GET',urlTemp);
+				var body = response.body.toString('utf-8');
+				var info = JSON.parse(body);
+				var items;
+				if (info.items)
+					for (var t = 0; t < info.items.length; t++)
+					{
+						outputElement(info.items[t], circle.loncenter,circle.latcenter);
+					}
+				nextPageToken = info.nextPageToken;
+			}
+		}
+		else if (info.items.length > 0)
+		{
+			//we have items! so we need to split the box into quads
+			q1lonmin = lonmin;
+			q1latmin = latmin;
+			q1lonmax = (lonmax+lonmin)/2;
+			q1latmax = (latmax+latmin)/2;
+			findAllVideosBBox(q1lonmin,q1latmin,q1lonmax,q1latmax,pubDate);
+	
+			q2lonmin = lonmin;
+			q2latmin = (latmax+latmin)/2;
+			q2lonmax = (lonmax+lonmin)/2;
+			q2latmax = latmax;
+			findAllVideosBBox(q2lonmin,q2latmin,q2lonmax,q2latmax,pubDate);
+	
+			q3lonmin = (lonmax+lonmin)/2;;
+			q3latmin = latmin;
+			q3lonmax = lonmax;
+			q3latmax = (latmax+latmin)/2;
+			findAllVideosBBox(q3lonmin,q3latmin,q3lonmax,q3latmax,pubDate);
+	
+			q4lonmin = (lonmax+lonmin)/2;
+			q4latmin = (latmax+latmin)/2;
+			q4lonmax = lonmax;
+			q4latmax = latmax;
+			findAllVideosBBox(q4lonmin,q4latmin,q4lonmax,q4latmax,pubDate);
+		}
+	
+	}
+	return;
+}
+
+
+//start the app, make an initial call to get all the videos since our start time
+console.log('id\tlongitude\tlatitude\ttitle\turl\tdateTime\tfoundTime');
+findAllVideosBBox(lonmin,latmin,lonmax,latmax,startTime);
+
+//....now go to sleep for gHours and wakeup and find the new videos
+var hour = 1000*60*60;
+var d1 = new Date();
+d1 = new Date(d1.getTime()-(gHours*hour));
+
+//set the timer to go off every gHours!
+setInterval(function() {
+	
+	findAllVideosBBox(lonmin,latmin,lonmax,latmax,d1.toISOString());
+	d1 = new Date();
+	d1 = new Date(d1.getTime()-(gHours*hour));
+	
+	}, gHours * hour);
+
+
+
